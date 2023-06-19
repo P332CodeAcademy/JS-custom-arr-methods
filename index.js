16678cd37b860aa210202802fda1516aefef11e1
JekaH1 <ceyhun100203@gmail.com>
2023-06-19 15:36:36 +0400

    task done

---
+++ index.js
@@ -0,0 +1,17 @@
+const numbers = ["salam", "necesen"];
+
+let num = numbers.find((element, idx, array) => {
+    // console.log(idx)
+    console.log(element)
+    // console.log(array)
+    // return element == "salam";
+});
+// console.log(num);
+
+function customFind(value, index, array, itirationArr) {
+
+    for (let i = 0; i < itirationArr.length; i++) {
+
+
+    }
+}
